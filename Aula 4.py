1f396c508d909d4f0813edcc5f470ffbebf64af1
monicarezende <34513636+monicarezende@users.noreply.github.com>
2017-12-15 15:01:20 -0200

    Aula 4 - final

--- Aula 4.py
+++ Aula 4.py
@@ -55,26 +55,23 @@ else:
 # 1 - Peça para o usuário digitar 3 números, envie-os para uma função chamada 'pegar_maior_valor' utilizando o método '*args' e retorne o maior deles utilizando a função max()
 # 2 - Peça para o usuário digitar 3 números, envie-os para uma função chamada 'pegar_menor_valor' utilizando o método '*args' e retorne o menor deles utilizando a função min()
 # 3 - Peça para o usuário digitar 3 números positivos e 3 números negativos, extraia o módulo (versão positiva) de cada número utilizando a função abs() e exiba na tela o maior e o menor número (utilize as funções criadas anteriormente no exercício 1 e 2).
-# 4 - Declare uma função que recebe um número e o formata de acordo com o tipo de sua variável (utilize a função type())
-# - Se a variável for um float, formate-a como moeda, exemplo:
-# - Entra a variável float 20.0 e exibe na tela R$ 20,00. Regra de formatação: {:.2f}, onde 2 representa o número e casas decimais.
-# - Se a variável for um int, formate-a deixando com pelo menos 5 dígitos (colocando zeros na frente). Regra de formatação: {:02d}, onde 2 representa o número de dígitos.
-# - Se a variável for uma string, formata-a para que utilize 10 espaços adicionais à esquerda. Regra de formatação: {:>1}, onde 1 representa o número de espaços que irá utilizar.
-# - Caso a variável não seja nenhuma dessas, exiba a mensagem "Esse tipo de variável não está mapeado."
 
 
+"""
 def funcao():
     return int(input("Insira um número: "))
 
 
-def negativo():
-    numero = int(input("Agora, insira um número negativo: "))
-    if numero < 0:
-        abs(numero)
-    else:
-        print("Seu número não é negativo. Tente novamente!")
-        return negativo()
+def inserir_negativo():
+    return int(input("Agora, insira um número negativo: "))
 
+
+def negativo(n):
+    if n < 0:
+        return abs(n)
+    else:
+        print("Um dos números não é negativo. Tente novamente!")
+        return inserir_negativo()
 
 
 def pegar_maior_valor(*args):
@@ -93,17 +90,44 @@ print("O maior número é {}.".format(maior_valor))
 menor_valor = pegar_menor_valor(n1, n2, n3)
 print("O menor número é {}.".format(menor_valor))
 
-n4 = negativo()
-n5 = negativo()
-n6 = negativo()
 
-maior_valor2 = pegar_maior_valor(n1, n2, n3, n4, n5, n6)
-print("O maior número é {}.".format(maior_valor2))
-menor_valor2 = pegar_menor_valor(n1, n2, n3, n4, n5, n6)
-print("O maior número é {}.".format(maior_valor2))
+n4 = inserir_negativo()
+n5 = inserir_negativo()
+n6 = inserir_negativo()
+n4_abs = negativo(n4)
+n5_abs = negativo(n5)
+n6_abs = negativo(n6)
 
-# TERMINAR EXERCÍCIO 18. ITEM 3 EM DIANTE
+maior_valor2 = pegar_maior_valor(n1, n2, n3, n4_abs, n5_abs, n6_abs)
+print("Considerando os novos números digitados, o maior número é {}.".format(maior_valor2))
+menor_valor2 = pegar_menor_valor(n1, n2, n3, n4_abs, n5_abs, n6_abs)
+print("Considerando os novos números digitados, o menor número é {}.".format(menor_valor2))
 
+
+# 4 - Declare uma função que recebe um número e o formata de acordo com o tipo de sua variável (utilize a função type())
+# - Se a variável for um float, formate-a como moeda, exemplo:
+# - Entra a variável float 20.0 e exibe na tela R$ 20,00. Regra de formatação: {:.2f}, onde 2 representa o número e casas decimais.
+# - Se a variável for um int, formate-a deixando com pelo menos 5 dígitos (colocando zeros na frente). Regra de formatação: {:02d}, onde 2 representa o número de dígitos.
+# - Se a variável for uma string, formata-a para que utilize 10 espaços adicionais à esquerda. Regra de formatação: {:>1}, onde 1 representa o número de espaços que irá utilizar.
+# - Caso a variável não seja nenhuma dessas, exiba a mensagem "Esse tipo de variável não está mapeado."
+
+
+def tipo_variavel(variavel):
+    if type(variavel) == float:
+        print("O número digitado {:.2f} é do tipo float.".format(variavel))
+    elif type(variavel) == int:
+        print("O número digitado {:05d} é do tipo inteiro.".format(variavel))
+    elif type(variavel) == str:
+        print("O número digitado {:>10} é do tipo string.".format(variavel))
+    else:
+        print("Esse tipo de variável não está mapeado.")
+
+
+tipo_variavel(0.6)
+tipo_variavel(6)
+tipo_variavel("6")
+tipo_variavel(True)
+"""
 
 """
 animais = "catdogfrog"
